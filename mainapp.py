158985b88a9ea121ede9d9d5fcc8a33e492c7516
halittosun38 <halittosun38@hotmail.com>
2026-03-30 03:18:35 +0100

    Update

--- mainapp.py
+++ mainapp.py
@@ -1,1 +1,10 @@
-print("hello")
+from tkinter import *
+from tkmacosx import *
+root = Tk()
+root.title("Assestment App")
+root.geometry("720x1280")
+label1=Label(root,text="Test")
+entry1 = Entry(root)
+entry1.pack
+
+root.mainloop()
