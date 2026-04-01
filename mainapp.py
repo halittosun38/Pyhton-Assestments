c04858971cc9b6886c80681c0b8e65df49363687
halittosun38 <halittosun38@hotmail.com>
2026-04-01 18:30:30 +0100

    Update

--- mainapp.py
+++ mainapp.py
@@ -14,13 +14,20 @@ def adddata(data):
     with open("data.txt", "w") as files:
         files.write(str(data)+" |")
 
-def createpopup():
+def createpopup1():
+
     root.attributes('-alpha',0.6)
     pop = Toplevel(root)
+    pop.geometry("300x200")
     pop.config(bg="white", highlightbackground="#000000", highlightthickness=2)
+    def closepopup1():
+        root.attributes('-alpha',1)
+        pop.destroy()
+    button2 = Button(pop,command=closepopup1,text="sss")
+    button2.pack()
+
     
-    
-button1 = Button(root,command=createpopup,text="Button For blur")
+button1 = Button(root,command=createpopup1,text="Button For blur")
 button1.pack(padx=20, pady=20)
 root.mainloop()
 
