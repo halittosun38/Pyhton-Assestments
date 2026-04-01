2e241155f6b151a78fe82250ceec6123880e3d2c
halittosun38 <halittosun38@hotmail.com>
2026-04-01 17:27:09 +0100

    Update

--- mainapp.py
+++ mainapp.py
@@ -8,4 +8,15 @@ label1.pack()
 entry1 = Entry(root)
 entry1.pack()
 
+
+def adddata(data):
+    with open("data.txt", "w") as files:
+        files.write(str(data)+" |")
+
+def createpopup():
+    root.attributes('-alpha',0.6)
+    
+button1 = Button(root,command=createpopup,text="Button For blur")
+button1.pack(padx=20, pady=20)
 root.mainloop()
+
